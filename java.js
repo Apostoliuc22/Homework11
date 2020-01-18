93e363b836f8aa4e2ca4e86f9287332cec99e547
Apostoliuc22 <apostoliuc.alex@gmail.com>
2020-01-18 21:54:32 +0200

    Homework11

---
+++ java.js
@@ -0,0 +1,35 @@
+var day = prompt('What day is today');
+
+switch (day) {
+  case 'Monday':
+    alert('Working Day');
+    break;
+    
+case 'Tuesday':
+    alert('Working Day');
+    break;
+
+case 'Wednesday':
+    alert('Working Day');
+    break;
+    
+case 'Thursday':
+    alert('Working Day');
+    break;
+        
+case 'Friday':
+    alert('Working Day');
+    break;
+      
+case 'Saturday':
+    alert('Weekend');
+    break;
+
+case 'Sunday':
+    alert('Weekend');
+    break;
+
+       default:
+           alert('Can not define, please try another value')   
+
+}
